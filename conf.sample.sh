4d31dd9c6c3c9a20aaeb31784e6fa3621a14847f
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2017-02-10 12:27:42 +0100

    Initial commit!

---
+++ conf.sample.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# enter values and rename this file to 'conf.sh'
+
+export TELEGRAM_TOKEN=""
+export TUMBLR_CONSUMER_KEY=""
+export TUMBLR_CONSUMER_SECRET=""
+export TUMBLR_OAUTH_TOKEN=""
+export TUMBLR_OAUTH_SECRET=""
+export TUMBLR_BLOG_NAME=""
+export TUMBLR_BLOG_STATE=""
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd "$(dirname "$0")" || exit 2; pwd)
+CONFIG=${CONFIG-"$THIS_DIR/conf.sh"}
+PYTHON3=${PYTHON3-"/usr/bin/env python3"}
+TELETYPER=${TELETYPER-"$THIS_DIR/teletyper.py"}
+
+pgrep -f "$TELETYPER" && exit 0
+
+# shellcheck source=conf.sh
+[[ -f "$CONFIG" ]] && . "$CONFIG"
+
+$PYTHON3 "$TELETYPER" \
+    --telegram-token="$TELEGRAM_TOKEN" \
+    --tumblr-consumer-key="$TUMBLR_CONSUMER_KEY" \
+    --tumblr-consumer-secret="$TUMBLR_CONSUMER_SECRET" \
+    --tumblr-oauth-token="$TUMBLR_OAUTH_TOKEN" \
+    --tumblr-oauth-secret="$TUMBLR_OAUTH_SECRET" \
+    --tumblr-blog-name="$TUMBLR_BLOG_NAME" \
+    --tumblr-blog-state="$TUMBLR_BLOG_STATE" \
+
+exit $?
